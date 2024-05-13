057c2e09d79e5689fc44e416480fe71ac792d27b
Alvaro-vaio <148105926+Alvaro-vaio@users.noreply.github.com>
2024-05-13 19:08:30 -0400

    endpoin6
    Buscar por Procedencia el repuesto

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const PORT = 3000;
 
 //array
 let repuestosAutomoviles = [
-    {id: 1 , codigo: '304', marca: 'toyo', tipo: 'rodamiento', procendencia: 'china', precio: '200', cantidad: '300' },
+    {id: 1 , codigo: '304', marca: 'toyo', tipo: 'rodamiento', procendencia: 'Japon', precio: '200', cantidad: '300' },
     {id: 2 , codigo: '045', marca: 'nck', tipo: 'cruceta', procendencia: 'china', precio: '200', cantidad: '300' },
-    {id: 3 , codigo: '837', marca: 'toyoki', tipo: 'amortiguador', procendencia: 'china', precio: '200', cantidad: '300' },
+    {id: 3 , codigo: '837', marca: 'toyoki', tipo: 'amortiguador', procendencia: 'Brasil', precio: '200', cantidad: '300' },
     {id: 4 , codigo: '657', marca: 'tokiko', tipo: 'muñon de suspencion', procendencia: 'china', precio: '200', cantidad: '300' },
-    {id: 5 , codigo: '390', marca: 'tree five', tipo: 'muñon de direccion', procendencia: 'china', precio: '200', cantidad: '300' },
+    {id: 5 , codigo: '390', marca: 'tree five', tipo: 'muñon de direccion', procendencia: 'Argentina', precio: '200', cantidad: '300' },
     {id: 6 , codigo: '454', marca: 'ichivan', tipo: 'homocinetico', procendencia: 'china', precio: '200', cantidad: '300' },
 ];
 //manejo JSON
@@ -55,6 +55,17 @@ app.delete('/eliminar-repuesto/:id', (req,res) => {
     reAutomovil = repuestosAutomoviles.filter(repuesto => repuesto.id !== id);
     res.status(201).json({mensaje : 'Se elimino Correctamente en Repuesto de la Lista'});
     console.log(reAutomovil);
+});
+//endpoint 6
+app.get('/repuesto-procedencia/:procendencia', (req, res) => {
+    const pais = req.params.procendencia;
+    const repprocendencia = repuestosAutomoviles.filter(x => x.proPais === pais);
+    if (repprocendencia.length > 0) {
+        res.json(repprocendencia);
+    }
+    else {
+        res.status(404).json ({mensaje : 'No se encontraron repuestos con esa procedencia'});
+    }
 });
 app.listen(PORT, () => {
 console.log("Servidor Corriendo en el Puerto http://localhost:" + PORT);
